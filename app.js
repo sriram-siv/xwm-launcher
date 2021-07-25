b1d3bb7c722a294bdd565a219fe225dfb2b5985e
Sriram Sivarajah <sriramsivarajah@gmail.com>
2021-07-25 13:32:47 +0100

    Fixes synching bugs and applies other refactors from v1.01

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
-const init = () => {
-
-  const unityInstance = window.UnityLoader.instantiate(
-    'unityContainer',
-    'Build/x-wing-webgl.json',
-    {
-      onProgress: (instance, progress) => {
-        if (progress >= 1) {
-          setTimeout(() => instance.SendMessage('Loader', 'clientIsWebGL'), 5000)
-        }
-      }
-    }
-  )
-
-  const getClipboard = event => {
-    return event.clipboardData.getData('Text')
-  }
+createUnityInstance(document.querySelector("#unity-canvas"), {
+  dataUrl: "Build/x-wing-1_01.data.unityweb",
+  frameworkUrl: "Build/x-wing-1_01.framework.js.unityweb",
+  codeUrl: "Build/x-wing-1_01.wasm.unityweb",
+  streamingAssetsUrl: "StreamingAssets",
+  companyName: "Sriram Sivarajah",
+  productName: "X-Wing Miniatures",
+  productVersion: "1.01",
+})
+.then((unityInstance) => {
+  unityInstance.SendMessage("Loader", "clientIsWebGL")
 
   document.querySelector('body').addEventListener('paste', event => {
     unityInstance.SendMessage(
       'Loader',
       'pasteFromBrowser',
-      getClipboard(event)
+      event.clipboardData.getData("Text")
     )
   })
+});
 
-  setTimeout(() => {
-    window.alert('Load xws data from raithos.github.io or any other squad builder')
-  }, 1000)
-}
-
-window.addEventListener('DOMContentLoaded', init)
+setTimeout(() => {
+  window.alert('Load xws data from raithos.github.io or any other squad builder')
+}, 1000)
